3e224b237c68a92f43feeee43337b3d492ecde38
GeorgeHinch <george@georgehinch.com>
2015-12-23 13:31:15 -0800

    Guide info change

--- Windows 10 WMC/Windows 10 WMC/Guide.xaml.cs
+++ Windows 10 WMC/Windows 10 WMC/Guide.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Media;
 using Windows.UI;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -57,6 +58,11 @@ namespace Windows_10_WMC
 
             ChannelStatus.Text = Channel1_Name.Text;
             ChannelNumber.Text = Channel1_Number.Text;
+
+            Info_Image.Source = new BitmapImage(
+                        new Uri("ms-appx:/Assets/anderson_cooper_360_ver6_xlg.jpg"));
+            Info_Name.Text = "Anderson Cooper 360";
+            Info_Synops.Text = "Anderson Cooper goes beyond the headlines to tell stories from many points of view, so you can make up your own mind about the news.";
         }
 
         private void Row2_Tapped(object sender, TappedRoutedEventArgs e)
@@ -71,6 +77,11 @@ namespace Windows_10_WMC
 
             ChannelStatus.Text = Channel2_Name.Text;
             ChannelNumber.Text = Channel2_Number.Text;
+
+            Info_Image.Source = new BitmapImage(
+                        new Uri("ms-appx:/Assets/087854af82aaca641c46104ac4655241-18142.jpg"));
+            Info_Name.Text = "Property Brothers";
+            Info_Synops.Text = "The Property Brothers are determined to help couples find, buy and transform extreme fixer-uppers into the ultimate dream home. And since it's hard to see beyond a dated property's shortcomings, they're using state-of-the-art CGI to reveal their vision of the future. Can Drew Scott and Jonathan Silver Scott convince these hesitant home buyers to take a radical risk? And can they complete their ambitious project on time and on budget?";
         }
     }
 }
